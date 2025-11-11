12e9d95707442017cb4a0e8715f61fabf8049ab4
rubbie kelvin <rubbiekelvinvoltsman@gmail.com>
2025-11-11 06:44:58 +0100

    chore: adding settings list

--- src/app/settings.rs
+++ src/app/settings.rs
@@ -1,18 +1,52 @@
-use gpui::{Context, EventEmitter, ParentElement, Render, Styled, div};
+use gpui::{
+    App, AppContext, Context, Div, Entity, EventEmitter, ParentElement, Render, SharedString,
+    Styled, Window, div,
+};
 use gpui_component::{
-    Icon, TitleBar,
+    Icon, IconName, IndexPath, TitleBar,
     button::{Button, ButtonVariants},
+    label::Label,
+    list::{ListDelegate, ListItem, ListState},
 };
 
 use crate::events::navigation::{NavigationEvent, Screen};
 
-pub struct SettingScreen;
+pub struct SettingScreen {
+    preset_list: Entity<ListState<PresetListDelegate>>,
+}
 
 impl EventEmitter<NavigationEvent> for SettingScreen {}
 
 impl SettingScreen {
-    pub fn new(_cx: &mut Context<Self>) -> Self {
-        return SettingScreen;
+    pub fn new(cx: &mut Context<Self>, window: &mut Window) -> Self {
+        let preset_list = cx.new(|cx| {
+            ListState::new(
+                PresetListDelegate {
+                    items: vec![],
+                    selected_index: None,
+                },
+                window,
+                cx,
+            )
+        });
+
+        return SettingScreen { preset_list };
+    }
+
+    fn presets(&self) -> Div {
+        return div().child(
+            div()
+                .flex()
+                .flex_row()
+                .justify_between()
+                .items_center()
+                .child(Label::new("Presets"))
+                .child(Button::new("new-preset-button").icon(IconName::Plus)),
+        );
+    }
+
+    fn body(&self) -> Div {
+        return div().p_2().flex().flex_col().gap_4().child(self.presets());
     }
 }
 
@@ -22,19 +56,66 @@ impl Render for SettingScreen {
         _window: &mut gpui::Window,
         cx: &mut gpui::Context<Self>,
     ) -> impl gpui::IntoElement {
-        return div().size_full().flex().flex_col().child(
-            TitleBar::new().child(div().child("Settings")).child(
-                div().flex().items_center().gap_2().child(
-                    Button::new("settings")
-                        .icon(Icon::new(Icon::empty()).path("icons/x.svg"))
-                        .ghost()
-                        .on_click(cx.listener(|_this, _event, _window, cx| {
-                            cx.emit(NavigationEvent {
-                                screen: Screen::Timer,
-                            });
-                        })),
+        return div()
+            .size_full()
+            .flex()
+            .flex_col()
+            .child(
+                // title bar
+                TitleBar::new().child(div().child("Settings")).child(
+                    div().flex().items_center().gap_2().child(
+                        Button::new("settings")
+                            .icon(Icon::new(Icon::empty()).path("icons/x.svg"))
+                            .ghost()
+                            .on_click(cx.listener(|_this, _event, _window, cx| {
+                                cx.emit(NavigationEvent {
+                                    screen: Screen::Timer,
+                                });
+                            })),
+                    ),
                 ),
-            ),
-        );
+            )
+            .child(self.body().flex_grow());
+    }
+}
+
+struct PresetItem {
+    id: SharedString,
+    label: SharedString,
+}
+
+struct PresetListDelegate {
+    items: Vec<PresetItem>,
+    selected_index: Option<IndexPath>,
+}
+
+impl ListDelegate for PresetListDelegate {
+    type Item = ListItem;
+
+    fn items_count(&self, _section: usize, _cx: &App) -> usize {
+        self.items.len()
+    }
+
+    fn render_item(
+        &self,
+        ix: IndexPath,
+        _window: &mut Window,
+        _cx: &mut App,
+    ) -> Option<Self::Item> {
+        self.items.get(ix.row).map(|item| {
+            ListItem::new(ix)
+                .child(Label::new(item.label.clone()))
+                .selected(Some(ix) == self.selected_index)
+        })
+    }
+
+    fn set_selected_index(
+        &mut self,
+        ix: Option<IndexPath>,
+        _window: &mut Window,
+        cx: &mut Context<ListState<Self>>,
+    ) {
+        self.selected_index = ix;
+        cx.notify();
     }
 }
